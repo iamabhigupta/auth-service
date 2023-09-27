a7af398a44d3261550b547e2bbbc20acc12d6e8c
Abhishek Gupta <91587690+iamabhigupta@users.noreply.github.com>
2023-09-28 00:52:39 +0530

    test setup

---
+++ app.spec.ts
@@ -0,0 +1,15 @@
+import request from "supertest";
+
+import { add } from "./src/utils";
+import app from "./src/app";
+
+describe("App", () => {
+   it("should work", () => {
+      const result = add(7, 27);
+      expect(result).toBe(34);
+   });
+   it("should return 200 status", async () => {
+      const response = await request(app).get("/").send();
+      expect(response.statusCode).toBe(200);
+   });
+});
