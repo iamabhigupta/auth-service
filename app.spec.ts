9e3d57fee8ba3d623a0022e0769a443bb4b029e2
iamabhigupta <abhishekgupta.july@gmail.com>
2023-11-11 17:22:12 +0530

    add test setup

---
+++ app.spec.ts
@@ -0,0 +1,17 @@
+import request from "supertest";
+import app from "./src/app";
+
+const sum = (a: number, b: number): number => {
+  return a + b;
+};
+
+describe("App", () => {
+  it("shoul sum numbers", () => {
+    expect(sum(1, 2)).toBe(3);
+  });
+
+  it("should return 200 status", async () => {
+    const res = await request(app).get("/").send();
+    expect(res.statusCode).toBe(200);
+  });
+});
