d55d0a9f55308671ab01e939f323471947a21d5e
iamabhigupta <abhishekgupta.july@gmail.com>
2023-11-21 23:44:23 +0530

    write first test

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,18 @@
+import "reflect-metadata";
+
 import express, { NextFunction, Request, Response } from "express";
-import { logger } from "./config/logger";
 import { HttpError } from "http-errors";
+
+import { logger } from "./config/logger";
+import authRouter from "./routes/auth";
 
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Welcome");
 });
+
+app.use("/auth", authRouter);
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
