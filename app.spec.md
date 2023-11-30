86b4f5627f9ee7df3a0a75321156d8b733e2b85b
iamabhigupta <abhishekgupta.july@gmail.com>
2023-11-30 16:50:31 +0530

    add error handling and logging

--- app.spec.md
+++ app.spec.md
@@ -2,16 +2,16 @@ import request from "supertest";
 import app from "./src/app";
 
 const sum = (a: number, b: number): number => {
-  return a + b;
+return a + b;
 };
 
 describe("App", () => {
-  it("shoul sum numbers", () => {
-    expect(sum(1, 2)).toBe(3);
-  });
+it("shoul sum numbers", () => {
+expect(sum(1, 2)).toBe(3);
+});
 
-  it("should return 200 status", async () => {
-    const res = await request(app).get("/").send();
-    expect(res.statusCode).toBe(200);
-  });
+it("should return 200 status", async () => {
+const res = await request(app).get("/").send();
+expect(res.statusCode).toBe(200);
+});
 });
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,15 +1,19 @@
 import express from "express";
-import { AuthController } from "../controllers/AuthController";
-import { UserService } from "../services/UserService";
-import { User } from "../entity/User";
+
 import { AppDataSource } from "../config/data-source";
+import { logger } from "../config/logger";
+import { AuthController } from "../controllers/AuthController";
+import { User } from "../entity/User";
+import { UserService } from "../services/UserService";
 
 const router = express.Router();
 
 const userRepository = AppDataSource.getRepository(User);
 const userService = new UserService(userRepository);
-const authController = new AuthController(userService);
+const authController = new AuthController(userService, logger);
 
-router.post("/register", (req, res) => authController.register(req, res));
+router.post("/register", (req, res, next) =>
+  authController.register(req, res, next),
+);
 
 export default router;
