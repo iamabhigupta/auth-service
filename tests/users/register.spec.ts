ca4b86f5b9d47408f3240a415079d2e4ed6d8ab0
iamabhigupta <abhishekgupta.july@gmail.com>
2024-01-17 22:35:50 +0530

    test token generation

--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -4,6 +4,7 @@ import app from "../../src/app";
 import { AppDataSource } from "../../src/config/data-source";
 import { User } from "../../src/entity/User";
 import { Roles } from "../../src/constants";
+import { isJwt } from "../utils";
 
 describe("POST /auth/register", () => {
   let connection: DataSource;
@@ -148,6 +149,45 @@ describe("POST /auth/register", () => {
       // Assert
       expect(response.statusCode).toBe(400);
       expect(users).toHaveLength(1);
+    });
+
+    it("should return the accessToken and refreshToken inside a cookie", async () => {
+      // Arrange
+      const userData = {
+        firstName: "Abhishek",
+        lastName: "Gupta",
+        email: "abhishek@codersgyan.com",
+        password: "password",
+      };
+
+      // Act
+      const response = await request(app).post("/auth/register").send(userData);
+
+      interface Headers {
+        "set-cookie": string[];
+      }
+
+      // Assert
+      let accessToken = null;
+      let refreshToken = null;
+
+      const cookies = (response.headers as Headers)["set-cookie"] || [];
+
+      cookies.forEach((cookie) => {
+        if (cookie.startsWith("accessToken=")) {
+          accessToken = cookie.split(";")[0].split("=")[1];
+        }
+
+        if (cookie.startsWith("refreshToken=")) {
+          refreshToken = cookie.split(";")[0].split("=")[1];
+        }
+      });
+
+      expect(accessToken).not.toBeNull();
+      expect(refreshToken).not.toBeNull();
+
+      expect(isJwt(accessToken)).toBeTruthy();
+      expect(isJwt(refreshToken)).toBeTruthy();
     });
   });
   describe("Fields are missing", () => {
