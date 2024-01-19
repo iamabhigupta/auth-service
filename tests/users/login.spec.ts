2bfb6ace36563b8bac8343adfbbc8895946cba12
iamabhigupta <abhishekgupta.july@gmail.com>
2024-01-19 19:20:47 +0530

    add login test

---
+++ tests/users/login.spec.ts
@@ -0,0 +1,150 @@
+import request from "supertest";
+import { DataSource } from "typeorm";
+import app from "../../src/app";
+import { AppDataSource } from "../../src/config/data-source";
+import { User } from "../../src/entity/User";
+import { Roles } from "../../src/constants";
+import { isJwt } from "../utils";
+import bcrypt from "bcrypt";
+
+describe("POST /auth/login", () => {
+  let connection: DataSource;
+
+  beforeAll(async () => {
+    connection = await AppDataSource.initialize();
+  });
+
+  beforeEach(async () => {
+    // Database truncate
+    await connection.dropDatabase();
+    await connection.synchronize();
+  });
+
+  afterAll(async () => {
+    await connection.destroy();
+  });
+
+  describe("Given all fields", () => {
+    it("should return the access token and refresh token inside a cookie", async () => {
+      // Arrange
+      const userData = {
+        firstName: "Rakesh",
+        lastName: "K",
+        email: "rakesh@mern.space",
+        password: "password",
+      };
+
+      const hashedPassword = await bcrypt.hash(userData.password, 10);
+
+      const userRepository = connection.getRepository(User);
+      await userRepository.save({
+        ...userData,
+        password: hashedPassword,
+        role: Roles.CUSTOMER,
+      });
+
+      // Act
+      const response = await request(app)
+        .post("/auth/login")
+        .send({ email: userData.email, password: userData.password });
+
+      interface Headers {
+        ["set-cookie"]: string[];
+      }
+      // Assert
+      let accessToken = null;
+      let refreshToken = null;
+      const cookies = (response.headers as Headers)["set-cookie"] || [];
+      cookies.forEach((cookie) => {
+        if (cookie.startsWith("accessToken=")) {
+          accessToken = cookie.split(";")[0].split("=")[1];
+        }
+
+        if (cookie.startsWith("refreshToken=")) {
+          refreshToken = cookie.split(";")[0].split("=")[1];
+        }
+      });
+      expect(accessToken).not.toBeNull();
+      expect(refreshToken).not.toBeNull();
+
+      expect(isJwt(accessToken)).toBeTruthy();
+      expect(isJwt(refreshToken)).toBeTruthy();
+    });
+
+    it("should return the 400 if email or password is wrong", async () => {
+      // Arrange
+      const userData = {
+        firstName: "Rakesh",
+        lastName: "K",
+        email: "rakesh@mern.space",
+        password: "password",
+      };
+
+      const hashedPassword = await bcrypt.hash(userData.password, 10);
+
+      const userRepository = connection.getRepository(User);
+      await userRepository.save({
+        ...userData,
+        password: hashedPassword,
+        role: Roles.CUSTOMER,
+      });
+
+      // Act
+      const response = await request(app)
+        .post("/auth/login")
+        .send({ email: userData.email, password: "wrongPassword" });
+      // Assert
+      expect(response.statusCode).toBe(400);
+    });
+  });
+
+  describe("Fields are not in proper format", () => {
+    it("should return 400 status code if email is not a valid email", async () => {
+      // Arrange
+      const userData = {
+        email: "codersgyan",
+        password: "password",
+      };
+      // Act
+      const response = await request(app).post("/auth/login").send(userData);
+
+      // Assert 1
+      expect(response.statusCode).toBe(400);
+      const userRepository = connection.getRepository(User);
+      const users = await userRepository.find();
+      expect(users).toHaveLength(0);
+    });
+
+    it("should return 400 status code if password length is less than 8 character", async () => {
+      // Arrange
+      const userData = {
+        email: " abhishek@codersgyan.com ",
+        password: "pass",
+      };
+      // Act
+      const response = await request(app).post("/auth/login").send(userData);
+
+      // Assert 1
+      expect(response.statusCode).toBe(400);
+      const userRepository = connection.getRepository(User);
+      const users = await userRepository.find();
+      expect(users).toHaveLength(0);
+    });
+
+    it("shoud return an array of error messages if email is missing", async () => {
+      // Arrange
+      const userData = {
+        email: "",
+        password: "password", // less than 8 chars
+      };
+      // Act
+      const response = await request(app).post("/auth/login").send(userData);
+
+      // Assert
+      expect(response.body).toHaveProperty("errors");
+      expect(
+        (response.body as Record<string, string>).errors.length,
+      ).toBeGreaterThan(0);
+    });
+  });
+});
