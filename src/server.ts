1447aa6b8a22ad361726cb89c49612b7af741b2b
iamabhigupta <abhishekgupta.july@gmail.com>
2024-07-11 00:26:30 +0530

    add logger

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,20 @@
 import app from "./app";
 import { Config } from "./config";
+import logger from "./config/logger";
 
 const startServer = () => {
    const { PORT } = Config;
    try {
       app.listen(PORT, () => {
-         // eslint-disable-next-line no-console
-         console.log(`Listening on port http://localhost:${PORT}`);
+         logger.info(`Listening on port http://localhost:${PORT}`);
       });
    } catch (error) {
-      // eslint-disable-next-line no-console
-      console.log(error);
-      process.exit(1);
+      if (error instanceof Error) {
+         logger.error(error.message);
+         setTimeout(() => {
+            process.exit(1);
+         }, 1000);
+      }
    }
 };
 
