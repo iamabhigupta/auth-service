443537f139bb35a2a442a0ba87155b14807c2d48
iamabhigupta <abhishekgupta.july@gmail.com>
2024-07-14 14:25:56 +0530

    add global error handling

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,21 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import logger from "./config/logger";
+import { HttpError } from "http-errors";
 
 const app = express();
 
 // Route
 app.get("/", (req, res) => {
    res.send("Abhishek Gupta");
+});
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+   logger.error(err.message);
+   const statusCode = err.statusCode || 500;
+   res.status(statusCode).json({
+      errors: [{ type: err.name, msg: err.message, path: "", location: "" }],
+   });
 });
 
 export default app;
